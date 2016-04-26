b63bb4d05bb44052efccefc90bf53a94aa1932af
Nelaneli <nelaneliyuna@gmail.com>
2016-04-26 23:05:16 +0700

    ini kurang lagi -_- maaf maaf

--- Chat/KIJ_Chat_Client/src/kij_chat_client/Write.java
+++ Chat/KIJ_Chat_Client/src/kij_chat_client/Write.java
@@ -5,10 +5,22 @@
  */
 package kij_chat_client;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
@@ -22,6 +34,8 @@ public class Write implements Runnable {
         ArrayList<String> log;
         String ciper;
         String siapkirim;
+        String chp;
+        String pesansiap;
 	
 	public Write(Scanner chat, PrintWriter out, ArrayList<String> log)
 	{
@@ -54,10 +68,12 @@ public class Write implements Runnable {
                                     out.flush();
                                     break;
                                 case "pm":
-                                    break;
-                                default:
-                                    out.println(input);//SEND IT TO THE SERVER
-                                    out.flush();//FLUSH THE STREAM
+                                    String pesan = input.split(" ")[2];
+                                    chp = AesCtr(pesan);
+                                    pesansiap = input.split(" ")[0].toUpperCase() + " " + input.split(" ")[1] + " " + chp;
+                                    System.out.println("Yang dikirim ke server: " + pesansiap);
+                                    out.println(pesansiap);
+                                    out.flush();
                                     break;
                             //out.println(input);//SEND IT TO THE SERVER
                             //out.flush();//FLUSH THE STREAM
@@ -122,4 +138,32 @@ public class Write implements Runnable {
             
         }
 
+         public String AesCtr(String pesan) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException{
+            //Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); 
+            byte[] keyBytes = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
+            byte[] ivBytes = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x00, 0x01, 0x02, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
+           
+            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+            IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+            Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+            System.out.println("input : " + new String(pesan));
+            
+            cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+            byte[] pesanbyte = pesan.getBytes();
+            ByteArrayInputStream bIn = new ByteArrayInputStream(pesanbyte);
+            CipherInputStream cIn = new CipherInputStream(bIn, cipher);
+            ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+            
+            int ch;
+            while ((ch = cIn.read()) >= 0) {
+              bOut.write(ch);
+            }
+
+            byte[] cipherText = bOut.toByteArray();
+
+            System.out.println("cipher: " + new String(cipherText));
+            String cipp = cipherText.toString();
+            return cipp;
+           }
+        
 }
